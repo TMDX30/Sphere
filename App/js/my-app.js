bf2d6d0f9ef81f8c882378d0331df3f000acc80b
Thomas Dix <thomas.dix@ipo.gov.uk>
2016-11-11 11:37:02 +0000

    - Added first set of custom pages. - Included files required for installation

--- App/js/my-app.js
+++ App/js/my-app.js
@@ -1,48 +1,39 @@
 // Initialize your app
-var myApp = new Framework7();
+var sphereApp = new Framework7({
+    //enable Material theme
+    material: true,
+    swipePanel: 'left',
+    pushState: true,
+    hideNavbarOnPageScroll: true,
+
+    // Hide and show indicator during ajax requests
+    onAjaxStart: function(xhr) {
+        sphereApp.showIndicator();
+    },
+    onAjaxComplete: function(xhr) {
+        sphereApp.hideIndicator();
+    }
+});
 
 // Export selectors engine
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
+var mainView = sphereApp.addView('.view-main', {});
 
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+//Setup slider
+$$(document).on('pageInit', function(e) {
+    var mySwiper = sphereApp.swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        parallax: true
     });
 });
 
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
+//Login screen
+$$('.login-screen .button').on('click', function() {
+    var username = $$('.login-screen input[name="username"]').val();
+    var password = $$('.login-screen input[name="password"]').val();
+    sphereApp.alert('Username: ' + username + ', Password: ' + password, function() {
+        sphereApp.closeModal('.login-screen');
+    });
+});
